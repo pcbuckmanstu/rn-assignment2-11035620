977c711be7f9a7cf55de760290a61019692bde36
Pharrell Buckman <pcbuckman@st.ug.edu.gh>
2024-05-27 14:41:58 +0000

    make name bold

--- MyApp/App.js
+++ MyApp/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Pharrell Buckman.</Text>
+      <Text style={styles.text}>My name is  
+        <Text style={styles.boldText}> Pharrell Buckman</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +21,8 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
   }
 });
